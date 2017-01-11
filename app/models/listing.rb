6dc352b5a618c0044168c91fb282acd79ba32aab
Raghu Betina <raghu@firstdraft.com>
2017-01-11 18:09:33 +0000

    Direct association between listings and requests

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,9 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_many   :requests,
+             :dependent => :destroy
+
   has_many   :messages,
              :dependent => :nullify
 
