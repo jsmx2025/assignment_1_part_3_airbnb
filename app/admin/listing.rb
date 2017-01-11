b82e0f2e095eba900d64f02d4fb4a13260d4d101
Raghu Betina <raghu@firstdraft.com>
2017-01-11 18:10:02 +0000

    Whitelist columns in Listing for ActiveAdmin

--- app/admin/listing.rb
+++ app/admin/listing.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Listing do
 
+ permit_params :title, :location, :policies, :default_price_per_night, :host_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
