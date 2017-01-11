1590515c51391d4ac2db1f0615ee0037387c3105
Raghu Betina <raghu@firstdraft.com>
2017-01-11 18:10:53 +0000

    Whitelist columns in GTagRating for ActiveAdmin

--- app/admin/g_tag_rating.rb
+++ app/admin/g_tag_rating.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register GTagRating do
 
+ permit_params :accuracy, :communication, :cleanliness, :location, :check_in, :value, :rating_id, :host_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
