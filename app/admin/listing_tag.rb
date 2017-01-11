3ba8200e0639056111584ffe0c598ed7a44a4ce4
Raghu Betina <raghu@firstdraft.com>
2017-01-11 18:10:07 +0000

    Whitelist columns in ListingTag for ActiveAdmin

--- app/admin/listing_tag.rb
+++ app/admin/listing_tag.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ListingTag do
 
+ permit_params :private_room, :entire_home, :number_of_bedrooms, :number_of_bathrooms, :number_of_beds, :max_occupants, :city, :neighborhood, :listing_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
