002832c9d505a4b2f76f393cb09d3241bf03f306
Raghu Betina <raghu@firstdraft.com>
2017-01-11 18:09:33 +0000

    Direct association between ratings and h_tag_ratings

--- app/models/h_tag_rating.rb
+++ app/models/h_tag_rating.rb
@@ -1,6 +1,8 @@
 class HTagRating < ApplicationRecord
   # Direct associations
 
+  belongs_to :rating
+
   belongs_to :guest,
              :class_name => "Guestprofile"
 
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,10 @@
 class Rating < ApplicationRecord
   # Direct associations
 
+  has_one    :guest_tag_rating,
+             :class_name => "HTagRating",
+             :dependent => :destroy
+
   has_many   :tagsratings,
              :class_name => "GTagRating",
              :dependent => :destroy
