3a6b3a4ba7baa05eb85f62a3b439c5af7db3e069
Raghu Betina <raghu@firstdraft.com>
2017-01-11 18:10:30 +0000

    Whitelist columns in Guestprofile for ActiveAdmin

--- app/admin/guestprofile.rb
+++ app/admin/guestprofile.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Guestprofile do
 
+ permit_params :guest_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
