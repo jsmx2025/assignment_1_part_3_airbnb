3c6dfe3b524f198d6c1466c9e135e2997e67598c
Raghu Betina <raghu@firstdraft.com>
2017-01-11 18:09:33 +0000

    Direct association between guesthostprofiles and bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,9 @@
 class Booking < ApplicationRecord
   # Direct associations
 
+  belongs_to :guest,
+             :class_name => "Guesthostprofile"
+
   belongs_to :listing
 
   belongs_to :host,
--- app/models/guesthostprofile.rb
+++ app/models/guesthostprofile.rb
@@ -1,6 +1,10 @@
 class Guesthostprofile < ApplicationRecord
   # Direct associations
 
+  has_many   :bookings,
+             :foreign_key => "guest_id",
+             :dependent => :destroy
+
   belongs_to :guest,
              :class_name => "UserGuest",
              :foreign_key => "user_guest_id"
