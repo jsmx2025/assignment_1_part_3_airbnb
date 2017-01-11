e3fce35a2bf258896a982794cd1a792090e82535
Raghu Betina <raghu@firstdraft.com>
2017-01-11 18:09:33 +0000

    Direct association between listings and messages

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,9 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_many   :messages,
+             :dependent => :nullify
+
   has_many   :availabilities,
              :dependent => :nullify
 
