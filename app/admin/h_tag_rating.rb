4415b470b94c129ae0fe121a11656ad9067eeab6
Raghu Betina <raghu@firstdraft.com>
2017-01-11 18:10:58 +0000

    Whitelist columns in HTagRating for ActiveAdmin

--- app/admin/h_tag_rating.rb
+++ app/admin/h_tag_rating.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register HTagRating do
 
+ permit_params :overall_rating, :rating_id, :guest_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
